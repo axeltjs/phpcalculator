002f0e4fdbd21bb9b2f273954de99c15a2248980
axeltjs <axel@thortech.asia>
2020-04-18 19:00:38 +0800

    Add subtract feature

---
+++ src/Traits/CalculatorTrait.php
@@ -0,0 +1,59 @@
+<?php
+namespace Jakmall\Recruitment\Calculator\Traits;
+
+trait CalculatorTrait
+{
+    public function generateCalculationDescription(array $numbers, string $operator): string
+    {
+        $glue = sprintf(' %s ',$operator);
+
+        return implode($glue, $numbers);
+    }
+
+    /**
+     * @param array $numbers
+     *
+     * @return float|int
+     */
+    public function calculateAll(array $numbers, string $operator)
+    {
+        $number = array_pop($numbers);
+
+        if (count($numbers) <= 0) {
+            return $number;
+        }
+
+        return $this->calculate($this->calculateAll($numbers, $operator), $number, $operator);
+    }
+
+    /**
+     * @param int|float $number1
+     * @param int|float $number2
+     *
+     * @return int|float
+     */
+    public function calculate($number1, $number2, $operator)
+    {
+        switch ($operator) {
+            case '-':
+                return $number1 - $number2;
+                break;
+
+            case '*':
+                return $number1 * $number2;
+                break;
+
+            case '/':
+                return $number1 / $number2;
+                break;
+
+            case '^':
+                return $number1 ** $number2;
+                break;
+            
+            default:
+                return $number1 + $number2;
+                break;
+        }
+    }
+}
